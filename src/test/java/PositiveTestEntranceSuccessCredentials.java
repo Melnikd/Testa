6ef764721b3ba551aad227f1d9e77d8de996eae9
Melnikd <diana.melnyk@nure.ua>
2019-11-26 12:17:04 +0200

    renamed TestPositive and TestNegative

--- src/test/java/PositiveTestEntranceSuccessCredentials.java
+++ src/test/java/PositiveTestEntranceSuccessCredentials.java
@@ -11,14 +11,14 @@ import java.util.List;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
-public class TestPositive extends BaseTest {
+public class PositiveTest extends BaseTest {
 
 
     @Test(dataProvider = "successCredentials")
     public void happyRename(MakeUpCredentials readCredential) {
 
         assertTrue(Entrance.entrance(readCredential), "With positive data validation passed    " + readCredential.getLogin() + "  " + readCredential.getPassword());
-        assertFalse(Entrance.entrance(readCredential),"Validation failed with positive data    " + readCredential.getLogin() + "  " + readCredential.getPassword());
+       // assertFalse(Entrance.entrance(readCredential),"Validation failed with positive data    " + readCredential.getLogin() + "  " + readCredential.getPassword());
 //        if (Entrance.entrance(readCredential)) {
 //            System.out.println("With positive data validation passed " + readCredential.getLogin() + "  " + readCredential.getPassword());//
 //        } else {
